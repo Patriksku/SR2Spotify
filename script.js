b620059b0b62889ea7b5d00bc8a13a179313e26e
JackyTu99 <48712035+JackyTu99@users.noreply.github.com>
2020-01-04 21:55:31 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -3,29 +3,21 @@ const getP2 = document.querySelector('#get_p2');
 const getP3 = document.querySelector('#get_p3');
 const getP4 = document.querySelector('#get_p4');
 
-const sendHttpRequest = (method, url, data) => {
-    return fetch(url, {
-        method: method,
-        mode: "no-cors",
-        body: JSON.stringify(data),
-        headers: data ? { 'Content-Type': 'application/json', 'Accept': 'application/json' } : {}
-    }).then(response => {
-        if (response.status >= 400) {
-            // !response.ok
-            return response.json().then(errResData => {
-                const error = new Error('Something went wrong!');
-                error.data = errResData;
-                throw error;
-            });
-        }
-        return response.json();
-    });
-};
-
 const getData = (url) => {
-    sendHttpRequest('GET', url).then(url => {
-        console.log(url);
+    $(document).ready(function(){
+        $.ajax({
+            url: url
+        }).then(function(data) {
+            $('.channel_id').html("Kanal: " + data.channelname);
+            $('.title').html("Nuvarande låt:  " + data.previoustitle);
+            $('.artist').html("Artist:  " + data.previousartist);
+            $('.album').html("Album:  " + data.previousalbum);
+            $('.next_title').html("Nästa låt: " + data.nexttitle);
+            $('.next_artist').html("Nästa artist: " + data.nextartist);
+            $('.next_album').html("Nästa album:  " + data.nextalbum);
+        });
     });
+    console.log(url);
 };
 
 getP1.addEventListener('click', () => {
