dc1a804364667f2d1957d10ed1025a35869862f3
Blossomrose123 <46005880+Blossomrose123@users.noreply.github.com>
2020-01-08 04:28:40 +0100

    added href

--- src/main/java/Spotify/Beans/Album.java
+++ src/main/java/Spotify/Beans/Album.java
@@ -1,58 +1,69 @@
 package Spotify.Beans;
 
 /**
- * This class represents an Album object in Spotify.
+ * This class represents an Artist object in Spotify.
  * @author Sara Karic
  */
 
-public class Album extends Artist {
-    private String album_name;
-    private String album_id;
-    private String album_uri;
-    private String album_url;
-    private String album_type;
+public class Artist {
+    private String artist_name;
+    private String artist_id;
+    private String artist_uri;
+    private String artist_url;
+    public String h = "<a href=\"https://api.spotify.com/v1/artists/{id}";
+    public String h2 = "<a href=\"https://api.spotify.com/v1/album/{id}";// temporär lösning för track2
+    public Artist artist_array[];
 
+    public Artist(){}
 
-    public Album(){}
-
-    public String getAlbum_name(){
-        return album_name;
+    public String getArtist_name(){
+        return artist_name;
     }
 
-    public void setAlbum_name(String album_name) {
-        this.album_name = album_name;
+    public void setArtist_name(String artist_name) {
+        this.artist_name = artist_name;
     }
 
-    public String getAlbum_id(){
-        return  album_id;
+    public String getArtist_id(){
+        return  artist_id;
     }
 
-    public void setAlbum_id(String album_id) {
-        this.album_id = album_id;
+    public void setArtist_id(String artist_id) {
+        this.artist_id = artist_id;
     }
 
-    public String getAlbum_uri() {
-        return album_uri;
+    public String getArtist_uri() {
+        return artist_uri;
     }
 
-    public void setAlbum_uri(String album_uri) {
-        this.album_uri = album_uri;
+    public void setArtist_uri(String artist_uri) {
+        this.artist_uri = artist_uri;
     }
 
-    public String getAlbum_url() {
-        return album_url;
+    public String getArtist_url() {
+        return artist_url;
     }
 
-    public void setAlbum_url(String album_url) {
-        this.album_url = album_url;
+    public void setArtist_url(String artist_url) {
+        this.artist_url = artist_url;
     }
 
-    public String getAlbum_type() {
-        return album_type;
+    public String getH(){
+        return h;
     }
 
-    public void setAlbum_type(String album_type) {
-        this.album_type = album_type;
+    public void setH(String h) {
+        this.h = h;
+    }
+
+    //temporär lösning för track2
+    public String getH2(){
+        return h2;
+    }
+
+
+    public void setH2(String h2) {
+        this.h2 = h2;
     }
 
     public Artist[] getArtist_array(){
@@ -63,10 +74,9 @@ public class Album extends Artist {
         this.artist_array = artist_array;
     }
 
-
-
     public String toString() {
-        return album_name + "\n" + album_id + "\n" + album_uri + "\n" + album_url + "\n" + artist_array + "\n";
+        return artist_name + "\n" + artist_id + "\n" + artist_uri + "\n" + artist_url + "\n" + h + "\n" + h2 + "\n";
     }
 }
+
 
