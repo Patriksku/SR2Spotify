7c7761d0ab6e4d38f38e62ed4ef83b23c4e4f3f9
Racquel <reed.rose.rachel@hotmail.com>
2020-01-09 13:50:39 +0100

    merged with latest changes in master

---
+++ target/classes/public/script.js
@@ -0,0 +1,37 @@
+const getP1 = document.querySelector('#get_p1');
+const getP2 = document.querySelector('#get_p2');
+const getP3 = document.querySelector('#get_p3');
+const getP4 = document.querySelector('#get_p4');
+
+const getData = (url) => {
+    $(document).ready(function(){
+        $.ajax({
+            url: url
+        }).then(function(data) {
+            $('.channel_id').html("Kanal: " + data.channelname);
+            $('.title').html("Nuvarande låt:  " + data.previoustitle);
+            $('.artist').html("Artist:  " + data.previousartist);
+            $('.album').html("Album:  " + data.previousalbum);
+            $('.next_title').html("Nästa låt: " + data.nexttitle);
+            $('.next_artist').html("Nästa artist: " + data.nextartist);
+            $('.next_album').html("Nästa album:  " + data.nextalbum);
+        });
+    });
+    console.log(url);
+};
+
+getP1.addEventListener('click', () => {
+    getData('http://localhost:4567/api/sveriges-radio/getsongs/132')
+})
+
+getP2.addEventListener('click', () => {
+    getData('http://localhost:4567/api/sveriges-radio/getsongs/163')
+})
+
+getP3.addEventListener('click', () => {
+    getData('http://localhost:4567/api/sveriges-radio/getsongs/164')
+})
+
+getP4.addEventListener('click', () => {
+    getData('http://localhost:4567/api/sveriges-radio/getsongs/207')
+})
