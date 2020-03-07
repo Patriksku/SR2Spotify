b33b771c655ea6914552ebf98c0a4a451f79a1e1
JackyTu99 <48712035+JackyTu99@users.noreply.github.com>
2020-03-07 21:14:29 +0100

    Add files via upload
    some new changes

--- src/main/resources/public/script.js
+++ src/main/resources/public/script.js
@@ -1,4 +1,4 @@
-//Här skapas variabler för alla SR-radiostationer
+//Här hämtar vi våra låttexter från de låtar som vi kan hitta
 
 const getLyrics =(url)=> {
     $(document).ready(function(){
@@ -13,6 +13,8 @@ const getLyrics =(url)=> {
     });
     console.log(url)
 };
+
+//Här skapas variabler för alla SR-radiostationer
 
 const getP1 = document.querySelector('#get_p1');
 const getP2 = document.querySelector('#get_p2');
@@ -41,45 +43,29 @@ const getData = (url) => {
     console.log(url);
 };
 
-//Här skapas funktioner för att när användaren trycker på knappen, hämtar man data från servern
+//Här skapas funktioner för att när användaren trycker på knappen, hämtar man data från servern och då hämtar vi de olika kanalerna, låttexten till den aktuella låten och låt URI
 
 getP1.addEventListener('click', () => {
     getData('http://localhost:4567/api/v1/sveriges-radio/songs/132')
     getLyrics('http://localhost:4567/api/v1/lyrics/getLyricsRadio/132')
-    getTrackURI('http://localhost:4567/api/v1/spotify/getsearch')
 })
 
 getP2.addEventListener('click', () => {
     getData('http://localhost:4567/api/v1/sveriges-radio/songs/163')
     getLyrics('http://localhost:4567/api/v1/lyrics/getLyricsRadio/163')
-    getTrackURI('http://localhost:4567/api/v1/spotify/getsearch')
 })
 
 getP3.addEventListener('click', () => {
     getData('http://localhost:4567/api/v1/sveriges-radio/songs/164')
     getLyrics('http://localhost:4567/api/v1/lyrics/getLyricsRadio/164')
-    getTrackURI('http://localhost:4567/api/v1/spotify/getsearch')
 })
 
 getP4.addEventListener('click', () => {
     getData('http://localhost:4567/api/v1/sveriges-radio/songs/207')
     getLyrics('http://localhost:4567/api/v1/lyrics/getLyricsRadio/207')
-    getTrackURI('http://localhost:4567/api/v1/spotify/getsearch')
 })
 
-const getTrackURI =(url)=> {
-    $(document).ready(function(){
-        $.ajax({
-            dataType: 'json',
-            contentType: 'application/json; charset=utf-8',
-            type: 'GET',
-            url: url,
-        }).then(function(data) {
-            $('.my_track_uri').html('Låt URI: ' + data.track_uri);
-        });
-    });
-    console.log(url);
-};
+//Hämtar session ID och skriver ut den på webbsidan
 
 const getMySessionID = document.querySelector('#get_session_id');
 
@@ -97,32 +83,44 @@ const getSessionID = (url) => {
     console.log(url);
 };
 
+//Trycker man på session ID knappen får man ut session ID
+
 getMySessionID.addEventListener('click', () => {
     getSessionID('http://localhost:4567/api/v1/spotify/session')
 })
 
+//En funktion som möjliggör hämtandet av låt URI
+
+const getMyTrackURI = document.querySelector('#get_track_uri');
+
+const getTrackURI =(url)=> {
+    $(document).ready(function(){
+        $.ajax({
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            type: 'GET',
+            url: url,
+        }).done(function(data) {
+            $('.my_track_uri').html('Låt URI: ' + data.track_uri);
+        })
+        .fail(function(xhr, status, error) {
+          //Uppmanar användaren att logga in först
+          var errorMessage = xhr.statusText
+          alert('1. Logga in på spotify först! \n 2. Välj en kanal');
+        });
+    });
+    console.log(url);
+};
+
+//Hämtar track URI
+
+getMyTrackURI.addEventListener('click', () => {
+    getTrackURI('http://localhost:4567/api/v1/spotify/getsearch')
+})
+
 //Här skapas variabler för Spotify-spellistor
 
-const getMyPlayList_1 = document.querySelector('#get_playlist');
-const getMyPlayList_ID_1 = document.querySelector('#get_playlist');
-const getMyPlayList_2 = document.querySelector('#get_playlist');
-const getMyPlayList_ID_2 = document.querySelector('#get_playlist');
-const getMyPlayList_3 = document.querySelector('#get_playlist');
-const getMyPlayList_ID_3 = document.querySelector('#get_playlist');
-const getMyPlayList_4 = document.querySelector('#get_playlist');
-const getMyPlayList_ID_4 = document.querySelector('#get_playlist');
-const getMyPlayList_5 = document.querySelector('#get_playlist');
-const getMyPlayList_ID_5 = document.querySelector('#get_playlist');
-const getMyPlayList_6 = document.querySelector('#get_playlist');
-const getMyPlayList_ID_6 = document.querySelector('#get_playlist');
-const getMyPlayList_7 = document.querySelector('#get_playlist');
-const getMyPlayList_ID_7 = document.querySelector('#get_playlist');
-const getMyPlayList_8 = document.querySelector('#get_playlist');
-const getMyPlayList_ID_8 = document.querySelector('#get_playlist');
-const getMyPlayList_9 = document.querySelector('#get_playlist');
-const getMyPlayList_ID_9 = document.querySelector('#get_playlist');
-const getMyPlayList_10 = document.querySelector('#get_playlist');
-const getMyPlayList_ID_10 = document.querySelector('#get_playlist');
+const getMyPlaylist = document.querySelector('#get_playlist');
 
 //Här hämta vi data från vår JSON-fil och plockar ut det objekt som vi vill ska visa sig på sidan och vi bestämmer även vart objekten ska någonstans
 
@@ -133,111 +131,25 @@ const getPlaylist = (url) => {
             contentType: 'application/json; charset=utf-8',
             type: 'GET',
             url: url,
-        }).then(function(data) {
-            $('.my_playlist_1').html("Spellista:  " + data.arrayOfPlaylists[0].playlist_name);
-            $('.my_playlist_id_1').html("Spellista ID:  " + data.arrayOfPlaylists[0].playlist_id);
-            $('.my_playlist_2').html("Spellista:  " + data.arrayOfPlaylists[1].playlist_name);
-            $('.my_playlist_id_2').html("Spellista ID:  " + data.arrayOfPlaylists[1].playlist_id);
-            $('.my_playlist_3').html("Spellista:  " + data.arrayOfPlaylists[2].playlist_name);
-            $('.my_playlist_id_3').html("Spellista ID:  " + data.arrayOfPlaylists[2].playlist_id);
-            $('.my_playlist_4').html("Spellista:  " + data.arrayOfPlaylists[3].playlist_name);
-            $('.my_playlist_id_4').html("Spellista ID:  " + data.arrayOfPlaylists[3].playlist_id);
-            $('.my_playlist_5').html("Spellista:  " + data.arrayOfPlaylists[4].playlist_name);
-            $('.my_playlist_id_5').html("Spellista ID:  " + data.arrayOfPlaylists[4].playlist_id);
-            $('.my_playlist_6').html("Spellista:  " + data.arrayOfPlaylists[5].playlist_name);
-            $('.my_playlist_id_6').html("Spellista ID:  " + data.arrayOfPlaylists[5].playlist_id);
-            $('.my_playlist_7').html("Spellista:  " + data.arrayOfPlaylists[6].playlist_name);
-            $('.my_playlist_id_7').html("Spellista ID:  " + data.arrayOfPlaylists[6].playlist_id);
-            $('.my_playlist_8').html("Spellista:  " + data.arrayOfPlaylists[7].playlist_name);
-            $('.my_playlist_id_8').html("Spellista ID:  " + data.arrayOfPlaylists[7].playlist_id);
-            $('.my_playlist_9').html("Spellista:  " + data.arrayOfPlaylists[8].playlist_name);
-            $('.my_playlist_id_9').html("Spellista ID:  " + data.arrayOfPlaylists[8].playlist_id);
-            $('.my_playlist_10').html("Spellista:  " + data.arrayOfPlaylists[9].playlist_name);
-            $('.my_playlist_id_10').html("Spellista ID:  " + data.arrayOfPlaylists[9].playlist_id);
+        }).done(function(data) {
+            $('.my_playlist').html("Spellista:  " + data.arrayOfPlaylists[0].playlist_name + " Spellista ID: " + data.arrayOfPlaylists[0].playlist_id);
+        })
+        .fail(function(xhr, status, error) {
+          //Uppmanar användaren att logga in först
+          var errorMessage = xhr.statusText
+          alert('Logga in på Spotify först!');
         });
     });
     console.log(url);
 };
 
-getMyPlayList_1.addEventListener('click', () => {
-    getPlaylist('http://localhost:4567/api/v1/spotify/myplaylists/10')
+//Hämtar den första spellistan man har på spotify
+
+getMyPlaylist.addEventListener('click', () => {
+    getPlaylist('http://localhost:4567/api/v1/spotify/myplaylists/1')
 })
 
-getMyPlayList_ID_1.addEventListener('click', () => {
-    getPlaylist('http://localhost:4567/api/v1/spotify/myplaylists/10')
-})
-
-getMyPlayList_2.addEventListener('click', () => {
-    getPlaylist('http://localhost:4567/api/v1/spotify/myplaylists/10')
-})
-
-getMyPlayList_ID_2.addEventListener('click', () => {
-    getPlaylist('http://localhost:4567/api/v1/spotify/myplaylists/10')
-})
-
-getMyPlayList_3.addEventListener('click', () => {
-    getPlaylist('http://localhost:4567/api/v1/spotify/myplaylists/10')
-})
-
-getMyPlayList_ID_3.addEventListener('click', () => {
-    getPlaylist('http://localhost:4567/api/v1/spotify/myplaylists/10')
-})
-
-getMyPlayList_4.addEventListener('click', () => {
-    getPlaylist('http://localhost:4567/api/v1/spotify/myplaylists/10')
-})
-
-getMyPlayList_ID_4.addEventListener('click', () => {
-    getPlaylist('http://localhost:4567/api/v1/spotify/myplaylists/10')
-})
-
-getMyPlayList_5.addEventListener('click', () => {
-    getPlaylist('http://localhost:4567/api/v1/spotify/myplaylists/10')
-})
-
-getMyPlayList_ID_5.addEventListener('click', () => {
-    getPlaylist('http://localhost:4567/api/v1/spotify/myplaylists/10')
-})
-
-getMyPlayList_6.addEventListener('click', () => {
-    getPlaylist('http://localhost:4567/api/v1/spotify/myplaylists/10')
-})
-
-getMyPlayList_ID_6.addEventListener('click', () => {
-    getPlaylist('http://localhost:4567/api/v1/spotify/myplaylists/10')
-})
-
-getMyPlayList_7.addEventListener('click', () => {
-    getPlaylist('http://localhost:4567/api/v1/spotify/myplaylists/10')
-})
-
-getMyPlayList_ID_7.addEventListener('click', () => {
-    getPlaylist('http://localhost:4567/api/v1/spotify/myplaylists/10')
-})
-
-getMyPlayList_8.addEventListener('click', () => {
-    getPlaylist('http://localhost:4567/api/v1/spotify/myplaylists/10')
-})
-
-getMyPlayList_ID_8.addEventListener('click', () => {
-    getPlaylist('http://localhost:4567/api/v1/spotify/myplaylists/10')
-})
-
-getMyPlayList_9.addEventListener('click', () => {
-    getPlaylist('http://localhost:4567/api/v1/spotify/myplaylists/10')
-})
-
-getMyPlayList_ID_9.addEventListener('click', () => {
-    getPlaylist('http://localhost:4567/api/v1/spotify/myplaylists/10')
-})
-
-getMyPlayList_10.addEventListener('click', () => {
-    getPlaylist('http://localhost:4567/api/v1/spotify/myplaylists/10')
-})
-
-getMyPlayList_ID_10.addEventListener('click', () => {
-    getPlaylist('http://localhost:4567/api/v1/spotify/myplaylists/10')
-})
+//En funktion som sparar ner variablerna i input-fälten och skickar det till vår endpoint
 
 const postMyData = document.querySelector('#send');
 
@@ -245,11 +157,12 @@ const postData =(url)=> {
     var session = $('#session_id_my_input').val();
     var playlist = $('#playlist_id_my_input').val();
     var track = $('#song_uri_my_input').val();
-      $.post(url, {session_id: session, playlist_id: playlist, song_uri: track}, function(result) {
-      console.log(result)
+      $.post(url, {session_id: session, playlist_id: playlist, song_uri: track},function(result) {
+        alert(result);
     });
-    alert('Låten är nu tillagd på din lista!');
 };
+
+//Skickar datan till vår endpoint som gör att man kan lägga till en låt på sin spellista
 
 postMyData.addEventListener('click', () => {
     postData('http://localhost:4567/api/v1/spotify/addsongplaylist')
